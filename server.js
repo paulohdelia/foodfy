74800df70ebdcb933e5cdd737e7ea65bee1afdaf
paulodelia <paulohdelia@gmail.com>
2020-02-04 15:33:01 -0300

    feat: add node and nunjucks

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const server = express();
+const recipes = require('./data');
+
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server, 
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res){
+    
+    return res.render("home", { items: recipes });
+
+});
+
+server.listen(5000, function(){
+    console.log("server is running");
+})
