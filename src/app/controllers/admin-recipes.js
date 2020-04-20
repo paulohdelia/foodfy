e730674c5de3fea8a301528894a8ae92f14850cb
paulodelia <paulohdelia@gmail.com>
2020-04-20 16:44:24 -0300

    feat: create, remove, edit recipes using db

--- src/app/controllers/admin-recipes.js
+++ src/app/controllers/admin-recipes.js
@@ -1,67 +1,42 @@
 const data = require('../../../data.json');
-const fs = require('fs')
+const Admin = require('../models/Admin-recipes')
 
 exports.index = function (req, res) { // Mostrar a lista de receitas
-    return res.render("admin/list", { recipes: data.recipes})
+    Admin.all(function(recipes) {
+        return res.render("admin/list", { recipes });
+    })
 }
 
 exports.create = function (req, res) { // Mostrar formulário de nova receita
     return res.render("admin/create")
 }
 
-exports.show = function (req, res) { // Exibir detalhes de uma receita
-    const recipeIndex = req.params.id;
-    return res.render("admin/detail", { recipes: data.recipes[recipeIndex], recipeIndex})
-}
-
-exports.edit = function (req, res) { // Mostrar formulários de edição de receita
-    const recipeIndex = req.params.id;
-    return res.render("admin/edit", { recipes: data.recipes[recipeIndex], recipeIndex})
-}
-
 exports.post = function (req, res) { // Cadastrar nova receita
-    const recipe = {
-        ...req.body
-    }
-
-    data.recipes.push(recipe);
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send('Write error!')
-
-        return res.redirect(`/admin/recipes/${data.recipes.length-1}`)
+    Admin.create(req.body, function (recipe) {
+        return res.redirect(`/admin/recipes/${recipe.id}`)
     })
 }
 
-exports.put = function (req, res) { // Editar uma receita
-        
-    const recipe = {
-        ...req.body
-    }
+exports.show = function (req, res) { // Exibir detalhes de uma receita
+    Admin.find(req.params.id, function(recipe){
+        return res.render("admin/detail", { recipe})
+    })
+}
 
-    delete recipe.id
+exports.edit = function (req, res) { // Mostrar formulários de edição de receita
+    Admin.find(req.params.id, function(recipe){
+        return res.render("admin/edit", { recipe })
+    })
+}
 
-    data.recipes[req.body.id] = recipe;
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send('Write error!')
-
-        return res.redirect(`/admin/recipes/${req.body.id}`)
+exports.put = function (req, res) { // Editar uma receita  
+    Admin.update(req.body, function(id) {
+        return res.redirect(`/admin/recipes/${id}`);
     })
 }
 
 exports.delete = function (req, res) { // Deletar uma receita
-    
-    if (data.recipes.length == 1) {
-         data.recipes = []
-    } else {
-        data.recipes.splice(req.body.id, 1)
-    }
-    
-
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send('Write error!')
-
+    Admin.delete(req.body.id ,function() {
         return res.redirect(`/admin/recipes`)
     })
 }
--- src/app/models/Admin-recipes.js
+++ src/app/models/Admin-recipes.js
@@ -1,6 +1,15 @@
 const db = require('../../config/db');
+const { date } = require('../../lib/utils');
 
 module.exports = {
+    all(callback) {
+        db.query('SELECT * FROM recipes ORDER BY id DESC', function(err, results) {
+            if (err) throw `Database Error! ${err}`;
+
+            callback(results.rows)
+        })
+    },
+
     create(data, callback) {
         const query = `
             INSERT INTO recipes (
@@ -13,5 +22,61 @@ module.exports = {
             ) VALUES ($1, $2, $3, $4, $5, $6)
             RETURNING id
         `
+
+        const values = [
+            data.image,
+            data.title,
+            data.ingredients,
+            data.preparation,
+            data.information,
+            date(Date.now()).iso
+        ]
+
+        db.query(query, values, function(err, results) {
+            if (err) throw `Database Error! ${err}`;
+            
+            callback(results.rows[0])
+        })
+    },
+
+    find(id, callback) {
+        db.query('SELECT * FROM recipes WHERE id = $1', [id], function(err, results) {
+            if (err) throw `Database Error! ${err}`;
+            callback(results.rows[0])
+        });
+    },
+
+    update(data, callback) {
+        const query = `
+            UPDATE recipes SET
+                image=($1),
+                title=($2),
+                ingredients=($3),
+                preparation=($4),
+                information=($5)
+            WHERE id = $6
+        `
+
+        const values = [
+            data.image,
+            data.title,
+            data.ingredients,
+            data.preparation,
+            data.information,
+            data.id
+        ]
+
+        db.query(query, values, function(err, results){
+            if (err) throw `Database Error! ${err}`;
+
+            callback(data.id)
+        })
+    },
+
+    delete(id, callback) {
+        db.query('DELETE FROM recipes WHERE id = $1', [id], function(err, results) {
+            if (err) throw `Database Error! ${err}`;
+            callback()
+        });
     }
 }
