d4739788cb8d90c06138a9739ce17d34574bdb06
paulohdelia <paulohdelia@gmail.com>
2020-08-21 21:55:01 -0300

    refactor: files now use base model

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,36 +1,7 @@
-const db = require('../../config/db');
-const fs = require('fs');
+const Base = require('./Base');
+
+Base.init({ table: 'files' });
 
 module.exports = {
-    create({filename, path}) {
-        const query = `
-            INSERT INTO files (
-                name,
-                path
-            ) VALUES ($1, $2)
-            RETURNING id
-        `;
-
-        const values = [
-            filename,
-            path
-        ];
-
-        return db.query(query, values);
-    },
-    async delete(id) {
-        try {
-            const results = await db.query('SELECT * FROM files WHERE id = $1', [id])
-            const file = results.rows[0];
-
-        fs.unlinkSync(file.path)
-        } catch(err) {
-            console.error(err)
-        }
-
-        return db.query(`
-            DELETE FROM recipe_files where file_id = ${id};
-            DELETE FROM files where id = ${id};
-        `);
-    }
+    ...Base,
 }
